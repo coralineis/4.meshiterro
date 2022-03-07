5fefa6b67943515da96ead1189e2b949b6cf96d5
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-07 04:41:35 +0000

    Finish 4-16

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
